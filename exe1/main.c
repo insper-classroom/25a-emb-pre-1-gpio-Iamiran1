a2e2399a4f97df74967b6efb19202f16fad96d1c
Iamiran1 <vinicius.c.miranda@icloud.com>
2025-02-11 11:48:38 -0300

    Teste ex1

--- exe1/main.c
+++ exe1/main.c
@@ -9,26 +9,20 @@ int main() {
 
   gpio_init(LED_PIN_R);
   gpio_set_dir(LED_PIN_R, GPIO_OUT);
-
-  while (true) {
-    gpio_put(LED_PIN_R, 1);
-    sleep_ms(250);
-
-    gpio_put(LED_PIN_R, 0);
-    sleep_ms(250);
-  }
-
-  //Meu código
-
+  
   gpio_init(LED_PIN_Y);
   gpio_set_dir(LED_PIN_Y, GPIO_OUT);
 
   while (true) {
+    gpio_put(LED_PIN_R, 1);
     gpio_put(LED_PIN_Y, 1);
     sleep_ms(250);
 
+    gpio_put(LED_PIN_R, 0);
     gpio_put(LED_PIN_Y, 0);
     sleep_ms(250);
   }
+
+
 }
 
